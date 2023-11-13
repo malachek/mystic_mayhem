8152fc9dba8b019cff5a08db164fab0da462df77
MishteeS <mishteeshaw@gmail.com>
2023-11-13 12:04:02 -0800

    Spawn outside bounds

--- Assets/Scripts/Monster1Manager.cs
+++ Assets/Scripts/Monster1Manager.cs
@@ -10,7 +10,14 @@ public class Monster1Manager : MonoBehaviour
 
     bool spawned = false;
 
-    private void Update(){
+    private Vector2 screenBounds;
+
+    void Start(){
+        screenBounds = Camera.main.ScreenToWorldPoint(new Vector3(screenBounds.x, screenBounds.y, 0f));
+    }
+
+    void LateUpdate(){
+    
         if(!(spawned)){
             SpawnEnemy(spawnAmount);
             spawned = true;
@@ -19,13 +26,47 @@ public class Monster1Manager : MonoBehaviour
 
     private void SpawnEnemy(int amt){
         for (int i=0; i<amt; i++){
-            Vector3 position = new Vector3(
-                UnityEngine.Random.Range(-spawnArea.x, spawnArea.x),
-                UnityEngine.Random.Range(-spawnArea.y, spawnArea.y),
-                0f);
+
+            int direction = Random.Range(0, 4); //gets range between 0 and 3 inc
+
+            //0-left
+            //1-up
+            //2-right
+            //3-down
+
+
+            if (direction == 0){
+                Vector3 position = new Vector3(
+                    UnityEngine.Random.Range(screenBounds.x - 10, screenBounds.x-2),
+                    UnityEngine.Random.Range(-spawnArea.y, spawnArea.y),
+                    0f);
+                GameObject newEnemy = Instantiate(monster);
+                newEnemy.transform.position = position;
+            }else if(direction == 1){
+                Vector3 position = new Vector3(
+                    UnityEngine.Random.Range(-spawnArea.x, spawnArea.x),
+                    UnityEngine.Random.Range((screenBounds.y*-1) +10, (screenBounds.y*-1)+2),
+                    0f);
+                GameObject newEnemy = Instantiate(monster);
+                newEnemy.transform.position = position;
+            }else if(direction == 2){
+                Vector3 position = new Vector3(
+                    UnityEngine.Random.Range(screenBounds.x*-1 + 10, screenBounds.x*-1 +2),
+                    UnityEngine.Random.Range(-spawnArea.y, spawnArea.y),
+                    0f);
+                GameObject newEnemy = Instantiate(monster);
+                newEnemy.transform.position = position;
+            }else{
+                Vector3 position = new Vector3(
+                    UnityEngine.Random.Range(-spawnArea.x, spawnArea.x),
+                    UnityEngine.Random.Range(screenBounds.y -10, screenBounds.y -2),
+                    0f);
+                GameObject newEnemy = Instantiate(monster);
+                newEnemy.transform.position = position;
+            }
             
-            GameObject newEnemy = Instantiate(monster);
-            newEnemy.transform.position = position;
+            
+            
         }
 
     }
