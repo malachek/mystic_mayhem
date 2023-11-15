eaa7f636f7dde947f82f17f1f79caf4c3645b6e4
mivan <catnipiscatweed@gmail.com>
2023-11-14 21:15:02 -0800

    lags. a lot. enemies spawn outsid eof the maze? what hte derp.
    - enemies spawn outside of maze
    - enemy pathfinding is still ass
    - spells are BORING!!!!!!!!!!!

--- Assets/Scripts/Monster1Manager.cs
+++ Assets/Scripts/Monster1Manager.cs
@@ -35,36 +35,29 @@ public class Monster1Manager : MonoBehaviour
             //2-right
             //3-down
 
-
+            float x = 0f;
+            float y = 0f;
             if (direction == 0){
-                Vector3 position = new Vector3(
-                    UnityEngine.Random.Range(screenBounds.x - 10, screenBounds.x-2),
-                    UnityEngine.Random.Range(-spawnArea.y, spawnArea.y),
-                    0f);
-                GameObject newEnemy = Instantiate(monster);
-                newEnemy.transform.position = position;
+                x = UnityEngine.Random.Range(screenBounds.x - 10, screenBounds.x-2);
+                y = UnityEngine.Random.Range(-spawnArea.y, spawnArea.y);
+                
             }else if(direction == 1){
-                Vector3 position = new Vector3(
-                    UnityEngine.Random.Range(-spawnArea.x, spawnArea.x),
-                    UnityEngine.Random.Range((screenBounds.y*-1) +10, (screenBounds.y*-1)+2),
-                    0f);
-                GameObject newEnemy = Instantiate(monster);
-                newEnemy.transform.position = position;
+                x = UnityEngine.Random.Range(-spawnArea.x, spawnArea.x);
+                y = UnityEngine.Random.Range((screenBounds.y*-1) +10, (screenBounds.y*-1)+2);
+                
             }else if(direction == 2){
-                Vector3 position = new Vector3(
-                    UnityEngine.Random.Range(screenBounds.x*-1 + 10, screenBounds.x*-1 +2),
-                    UnityEngine.Random.Range(-spawnArea.y, spawnArea.y),
-                    0f);
-                GameObject newEnemy = Instantiate(monster);
-                newEnemy.transform.position = position;
+                x = UnityEngine.Random.Range(screenBounds.x*-1 + 10, screenBounds.x*-1 +2);
+                y = UnityEngine.Random.Range(-spawnArea.y, spawnArea.y);
+               
             }else{
-                Vector3 position = new Vector3(
-                    UnityEngine.Random.Range(-spawnArea.x, spawnArea.x),
-                    UnityEngine.Random.Range(screenBounds.y -10, screenBounds.y -2),
-                    0f);
-                GameObject newEnemy = Instantiate(monster);
-                newEnemy.transform.position = position;
+                x = UnityEngine.Random.Range(-spawnArea.x, spawnArea.x);
+                y = UnityEngine.Random.Range(screenBounds.y -10, screenBounds.y -2);
+                
             }
+            Vector3 position = new Vector3(x, y, 0f);
+            GameObject newEnemy = Instantiate(monster);
+            newEnemy.SetActive(true);
+            newEnemy.transform.position = position;
             
             
             
