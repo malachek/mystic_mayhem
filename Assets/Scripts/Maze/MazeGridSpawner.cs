b53613d08512509aab45f8b5b948ced6cccd5db7
gavindg <52291693+gavindg@users.noreply.github.com>
2023-11-19 22:26:11 -0800

    Remove MazeGrid
    removes mazegrid and references to it

--- Assets/Scripts/Maze/MazeGridSpawner.cs
+++ Assets/Scripts/Maze/MazeGridSpawner.cs
@@ -10,11 +10,4 @@ public class MazeGridSpawner : MonoBehaviour
     public GameObject origin;
 
     public Grid g; // sorry, i know this variable name is terrible. it's temporary though; the MazeGrid class will be phased out p soon.
-
-    [HideInInspector] public MazeGrid grid;
-
-    private void Awake()
-    {
-        grid = new MazeGrid(width, height, mazeCellSize, Vector3Int.CeilToInt(origin.transform.position));
-    }
 }
